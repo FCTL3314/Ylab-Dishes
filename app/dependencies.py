945d828af5137bf24be7451ed96620379d5f2e98
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-08-05 15:33:58 +0300

    Add dependencies type annotation

--- app/dependencies.py
+++ app/dependencies.py
@@ -14,42 +14,42 @@ from app.submenu.services import CachedSubmenuService, SubmenuService
 ActiveSession = Depends(get_async_session)
 
 
-def menu_service():
+def menu_service() -> MenuService[MenuResponse]:
     return MenuService[MenuResponse](MenuWithCountingRepository())
 
 
 ActiveMenuService = Depends(menu_service)
 
 
-def cached_menu_service():
+def cached_menu_service() -> CachedMenuService[MenuResponse]:
     return CachedMenuService[MenuResponse](MenuWithCountingRepository())
 
 
 ActiveCachedMenuService = Depends(cached_menu_service)
 
 
-def submenu_service():
+def submenu_service() -> SubmenuService[SubmenuResponse]:
     return SubmenuService[SubmenuResponse](SubmenuWithCountingRepository())
 
 
 ActiveSubmenuService = Depends(submenu_service)
 
 
-def cached_submenu_service():
+def cached_submenu_service() -> CachedSubmenuService[SubmenuResponse]:
     return CachedSubmenuService[SubmenuResponse](SubmenuWithCountingRepository())
 
 
 ActiveCachedSubmenuService = Depends(cached_submenu_service)
 
 
-def dish_service():
+def dish_service() -> DishService[DishBase]:
     return DishService[DishBase](DishRepository())
 
 
 ActiveDishService = Depends(dish_service)
 
 
-def cached_dish_service():
+def cached_dish_service() -> CachedDishService[DishBase]:
     return CachedDishService[DishBase](DishRepository())
 
 
